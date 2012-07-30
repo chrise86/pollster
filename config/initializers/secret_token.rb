59b89902b307b3d6ce47a1386da1b4ea555fdb65
Apprentice <apprentice@dbc08.local>
2012-07-30 10:38:24 -0700

    first commit, woot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PollsterJy::Application.config.secret_token = 'b794d5328cf5a7063d986b1b810cbc098ff466b75a1186dbace0c56d3a2e1b0e32a141e771b890e582bc498212c2bd3c13c492a73c4a8edc9db6155abef61be2'
