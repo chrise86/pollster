0caa27fc2f438137cc3f1092c311710e9453e4a4
Apprentice <apprentice@dbc08.local>
2012-07-31 16:07:25 -0700

    got responses to work

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -3,6 +3,16 @@ class ResponsesController < ApplicationController
     @poll = Poll.find(params[:poll_id])
   end
 
+  def create
+    params.each_pair do |k,v|
+      if k.include? "answer"
+        Answer.create(:question_id => k.split("-")[1], :text => v)
+      end
+    end
+    redirect_to poll_path(params[:poll_id])
+  end
+
   def show
   end
+
 end
